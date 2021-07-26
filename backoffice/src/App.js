29d2a97463ae55457e77d243820c867b141d9680
Thomas Lemoine <contact@thomaslemoine.net>
2021-07-26 14:58:05 +0200

    Add secure api users on backoffice

--- backoffice/src/App.js
+++ backoffice/src/App.js
@@ -1,13 +1,21 @@
 import * as React from "react";
-import {Admin, Resource, EditGuesser, initialState} from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import {Admin, Resource, fetchUtils} from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
 import {UserList, UserEdit} from './Users';
 import customRoutes from "./customRoutes";
 import authProvider from "./authProvider";
 import {Dashboard} from './Dashboard';
 
+const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({Accept: 'application/json'});
+    }
+    const token = localStorage.getItem('token');
+    options.headers.set('x-access-token', `${token}`);
+    return fetchUtils.fetchJson(url, options);
+};
 
-const dataProvider = jsonServerProvider("http://0.0.0.0:3000/api");
+const dataProvider = simpleRestProvider("http://0.0.0.0:3000/api", httpClient, 'X-Total-Count');
 const App = () => (
 
 <Admin dashboard={Dashboard} customRoutes={customRoutes} authProvider={authProvider} dataProvider={dataProvider}>
