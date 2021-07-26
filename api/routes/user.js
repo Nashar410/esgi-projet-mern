a3b53ebc9cfc0ac67cec386cfa7e6960c3bda9a0
Thomas Lemoine <contact@thomaslemoine.net>
2021-07-26 12:31:52 +0200

    Unsecure create user

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,7 +5,7 @@ module.exports = app => {
     var router = require("express").Router();
 
     // Create a new Users
-    router.post("/", [authJwt.verifyToken, authJwt.isAdmin], users.create);
+    router.post("/", users.create);
 
     // Retrieve all Users
     router.get("/", [authJwt.verifyToken, authJwt.isAdmin], users.findAll);
