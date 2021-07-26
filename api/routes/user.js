d065fda293fb227eb2af8b0fb1e851d9ab0f3f0b
Thomas Lemoine <contact@thomaslemoine.net>
2021-07-26 12:29:44 +0200

    Secure route API user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,25 +1,26 @@
 module.exports = app => {
     const users = require("../controllers/user.js");
+    const { authJwt } = require("../middleware");
 
     var router = require("express").Router();
 
     // Create a new Users
-    router.post("/", users.create);
+    router.post("/", [authJwt.verifyToken, authJwt.isAdmin], users.create);
 
     // Retrieve all Users
-    router.get("/", users.findAll);
+    router.get("/", [authJwt.verifyToken, authJwt.isAdmin], users.findAll);
 
     // Retrieve a single Users with id
-    router.get("/:id", users.findOne);
+    router.get("/:id", [authJwt.verifyToken, authJwt.isAdmin], users.findOne);
 
     // Update a Users with id
-    router.put("/:id", users.update);
+    router.put("/:id", [authJwt.verifyToken, authJwt.isAdmin], users.update);
 
     // Delete a Users with id
-    router.delete("/:id", users.delete);
+    router.delete("/:id", [authJwt.verifyToken, authJwt.isAdmin], users.delete);
 
     // Delete all Users
-    router.delete("/", users.deleteAll);
+    router.delete("/", [authJwt.verifyToken, authJwt.isAdmin], users.deleteAll);
 
     app.use('/api/users', router);
 };
