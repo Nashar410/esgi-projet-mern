957626be30ba3be9f439e1ce6a83c905528ab564
Thomas Lemoine <contact@thomaslemoine.net>
2021-07-28 09:09:04 +0200

    Display confirmed value on list of users

--- backoffice/src/Users.js
+++ backoffice/src/Users.js
@@ -1,17 +1,29 @@
 import * as React from "react";
-import { List, Datagrid, TextField, EmailField, EditButton, TextInput, BooleanInput, Edit, SimpleForm, DateInput } from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    EmailField,
+    EditButton,
+    TextInput,
+    BooleanInput,
+    BooleanField,
+    Edit,
+    SimpleForm,
+    DateInput
+} from 'react-admin';
 
 
 export const UserList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="firstName" />
-            <TextField source="lastName" />
-            <TextField source="username" />
-            <EmailField source="email" />
-            <TextField source="company" />
-            <EditButton />
+            <TextField source="id"/>
+            <TextField source="firstName"/>
+            <TextField source="lastName"/>
+            <TextField source="username"/>
+            <EmailField source="email"/>
+            <BooleanField source="confirmed"/>
+            <EditButton/>
         </Datagrid>
     </List>
 );
@@ -19,16 +31,16 @@ export const UserList = props => (
 export const UserEdit = props => (
     <Edit {...props}>
         <SimpleForm>
-            <TextInput source="firstName" />
-            <TextInput source="lastName" />
-            <TextInput source="email" />
-            <TextInput source="username" />
-            <DateInput source="kbis" />
-            <TextInput source="role" />
-            <TextInput source="devise" />
-            <TextInput source="contact" />
-            <TextInput source="company" />
-            <BooleanInput source="confirmed" />
+            <TextInput source="firstName"/>
+            <TextInput source="lastName"/>
+            <TextInput source="email"/>
+            <TextInput source="username"/>
+            <DateInput source="kbis"/>
+            <TextInput source="role"/>
+            <TextInput source="devise"/>
+            <TextInput source="contact"/>
+            <TextInput source="company"/>
+            <BooleanInput source="confirmed"/>
         </SimpleForm>
     </Edit>
 );
